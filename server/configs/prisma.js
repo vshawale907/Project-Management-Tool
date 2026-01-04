02308ba141c8d42be91e4860eb75682d6144b0a2
Vaibhav Shankar Hawale <vaibhavhawale907@gmail.com>
2026-01-04 15:17:12 +0530

    prims file

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -2,23 +2,27 @@ import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import { PrismaNeon } from '@prisma/adapter-neon';
 import { neonConfig } from '@neondatabase/serverless';
-
 import ws from 'ws';
+
 neonConfig.webSocketConstructor = ws;
 
-// To work in edge environments (Cloudflare Workers, Vercel Edge, etc.), enable querying over fetch
-// neonConfig.poolQueryViaFetch = true
+declare global {
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient | undefined;
+}
 
-// Type definitions
-// declare global {
-//   var prisma: PrismaClient | undefined
-// }
-
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 
 const adapter = new PrismaNeon({ connectionString });
-const prisma = global.prisma || new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV === 'development') global.prisma = prisma;
+const prisma =
+    global.prisma ||
+    new PrismaClient({
+        adapter,
+    });
+
+if (process.env.NODE_ENV === 'development') {
+    global.prisma = prisma;
+}
 
 export default prisma;
