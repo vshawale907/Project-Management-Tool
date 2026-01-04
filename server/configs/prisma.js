ac26103b358d4827c760bbe88c046420772619ed
Vaibhav Shankar Hawale <vaibhavhawale907@gmail.com>
2026-01-04 15:24:46 +0530

    fixed

--- server/configs/prisma.js
+++ server/configs/prisma.js
@@ -6,20 +6,11 @@ import ws from 'ws';
 
 neonConfig.webSocketConstructor = ws;
 
-declare global {
-    // eslint-disable-next-line no-var
-    var prisma: PrismaClient | undefined;
-}
-
-const connectionString = process.env.DATABASE_URL!;
+const connectionString = process.env.DATABASE_URL;
 
 const adapter = new PrismaNeon({ connectionString });
 
-const prisma =
-    global.prisma ||
-    new PrismaClient({
-        adapter,
-    });
+const prisma = global.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV === 'development') {
     global.prisma = prisma;
